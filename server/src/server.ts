f31baf4a3d6c37514c9b5327d0aa38ba4ed75a55
kvojps <joseferreira.santosjunior@upe.br>
2022-07-28 09:03:29 -0300

    adicionando server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,6 +4,13 @@ const app = express();
 
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.end('Bem vindo!');
+})
+
+app.use((req, res) => {
+  res.status(404).end('Pagina nao encontrada!')
+})
 
 app.listen(3333, () => {
   console.log('HTTP server running!');
