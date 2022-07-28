cc2b2671fa3f77318ba542fbc7b6d488da34a4e2
kvojps <joseferreira.santosjunior@upe.br>
2022-07-28 09:24:35 -0300

    adicionando metodo de delete do EventoController

--- server/src/controller/EventoController.ts
+++ server/src/controller/EventoController.ts
@@ -2,6 +2,8 @@ import express from 'express'
 
 const EventoController = express.Router()
 
+//Adicionar rota para criar evento
+
 EventoController.get('/eventos', (req, res) => {
     res.send("Listar todos os eventos.")
 })
@@ -10,3 +12,12 @@ EventoController.get('/eventos/:id', (req, res) => {
     const id: number = +req.params.id
     res.send(`Retorna o item específico ${id}`)
 })
+
+//Adicionar rota para atualizar evento
+
+EventoController.delete('/eventos/:id', (req, res) => {
+    const id: number = +req.params.id
+    res.send(`Apaga o item específico ${id}`)
+})
+
+export default EventoController
