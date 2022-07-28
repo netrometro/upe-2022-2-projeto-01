6f8a506f9519740229a69bc63065fc7dce3e002f
kvojps <joseferreira.santosjunior@upe.br>
2022-07-28 09:21:33 -0300

    adicionando rota para pegar envento especifico

--- server/src/controller/EventoController.ts
+++ server/src/controller/EventoController.ts
@@ -5,3 +5,8 @@ const EventoController = express.Router()
 EventoController.get('/eventos', (req, res) => {
     res.send("Listar todos os eventos.")
 })
+
+EventoController.get('/eventos/:id', (req, res) => {
+    const id: number = +req.params.id
+    res.send(`Retorna o item específico ${id}`)
+})
