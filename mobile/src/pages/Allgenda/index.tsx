909e8d17385909f80740d13aefd02b8a8416c7d9
Kvojps <josebazjunior09@gmail.com>
2022-09-23 13:19:01 -0300

    adicionando tela allgenda com componentes de anúncios e rota para cadastrar anuncio

--- mobile/src/pages/Allgenda/index.tsx
+++ mobile/src/pages/Allgenda/index.tsx
@@ -1,10 +1,16 @@
-import { View } from "react-native";
+import { View, Text, Touchable, TouchableOpacity } from "react-native";
 import { styles } from "./styles";
 
 export function Allgenda() {
     return (
         <View style={styles.container}>
-            
+            <View style={styles.ads}>
+                <Text style={styles.title}>Anúncios</Text>        
+            </View>
+
+            <TouchableOpacity style={styles.button}>
+                <Text style={styles.buttonText}>Cadastrar anúncio</Text>
+            </TouchableOpacity>
         </View>
     )
 }
--- mobile/src/pages/Allgenda/styles.ts
+++ mobile/src/pages/Allgenda/styles.ts
@@ -5,7 +5,38 @@ export const styles = StyleSheet.create({
     container: {
         backgroundColor: THEME.COLORS.BACKGROUND,
         flex: 1,
-        justifyContent: "center",
         alignItems: "center",
-    }
+    },
+    title: {
+        alignSelf: "center",
+        fontSize: THEME.FONT_SIZE.LG,
+        fontWeight: "bold",
+        marginTop: 20,
+        marginBottom: 12,
+    },
+    ads: {
+        backgroundColor: THEME.COLORS.BACKGROUND_FORM,
+        borderTopLeftRadius: 25,
+        borderTopRightRadius: 25,
+        borderBottomLeftRadius: 25,
+        borderBottomRightRadius: 25,
+        padding: "5%",
+        width: "80%",
+        height: "60%",
+        marginTop: "10%"
+    },
+    button: {
+        position: "absolute",
+        backgroundColor: THEME.COLORS.BACKGROUND_FORM,
+        borderRadius: 50,
+        paddingVertical: 8,
+        width: "80%",
+        alignSelf: "center",
+        bottom: "20%",
+        alignItems: "center",
+      },
+      buttonText: {
+        fontSize: 18,
+        fontWeight: "bold",
+      },
 })
